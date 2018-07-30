4f62e6bd95901c826341d656aaf262476c64254d
Aymen Chebbi <aymen.chebi@gmail.com>
2018-07-30 08:32:15 +0100

    Content and UI adjustements

--- src/components/Technologies/index.js
+++ src/components/Technologies/index.js
@@ -12,9 +12,13 @@ class Technologies extends Component {
               State of the art technologies
            </h2>
           </div>
-          <p>  Through  the different track proposed at GoMyCode, Students can build their own project using cutting edge technologies that help keep up with the latest trends in the IT industry.
-          <br/>
-          We state, for example;  React (Web Dev Track), Unity (Game Track), TensorFlow (AI Track)</p>
+          <p>We believe in the importance of being up-to-date when it comes to technology
+            Our programs rely on
+            <br />
+            Through  the different track proposed at GoMyCode, Students can build their own project using cutting edge technologies that helps keep up with the latest trends in the IT industry.
+            <br />
+            We state for exemple;  React (Web Dev Track) , Unity (Game Track), TensorFlow (AI Track)
+            </p>
           <div className="technologies">
             <svg viewBox="0 0 128 128">
               <path fill="#1572B6" d="M18.814 114.123l-10.054-112.771h110.48l-10.064 112.754-45.243 12.543-45.119-12.526z"></path><path fill="#33A9DC" d="M64.001 117.062l36.559-10.136 8.601-96.354h-45.16v106.49z"></path><path fill="#fff" d="M64.001 51.429h18.302l1.264-14.163h-19.566v-13.831h34.681999999999995l-.332 3.711-3.4 38.114h-30.95v-13.831z"></path><path fill="#EBEBEB" d="M64.083 87.349l-.061.018-15.403-4.159-.985-11.031h-13.882l1.937 21.717 28.331 7.863.063-.018v-14.39z"></path><path fill="#fff" d="M81.127 64.675l-1.666 18.522-15.426 4.164v14.39l28.354-7.858.208-2.337 2.406-26.881h-13.876z"></path><path fill="#EBEBEB" d="M64.048 23.435v13.831000000000001h-33.407999999999994l-.277-3.108-.63-7.012-.331-3.711h34.646zM64.001 51.431v13.831000000000001h-15.209l-.277-3.108-.631-7.012-.33-3.711h16.447z"></path>
