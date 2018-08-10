cf19cb9cefe8d20884f8c53eca1c2a370d427182
Aymen Chebbi <aymen.chebi@gmail.com>
2018-08-10 10:35:07 +0100

    Ui Fixes

--- src/components/ProjectsList/components/projectItem.js
+++ src/components/ProjectsList/components/projectItem.js
@@ -1,26 +1,24 @@
 import React from 'react'
-import ReactImageFallback from "react-image-fallback"
+import ReactImageFallback from 'react-image-fallback'
 import placeholder from './placeholder.png'
 import './style.css'
 
 const toTitleCase = (str = '') => str.replace(/\w\S*/g, txt => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase())
 
-export default ({ id, name, description, logo, keywords, openModal }) => {
-  return (
-    <div className="project-item" onClick={() => openModal(id)}>
-      <ReactImageFallback
-        src={logo}
-        fallbackImage={placeholder}
-        alt={name}
-        className='icon'
-      />
-      <div className="text-content">
-        <h4 className="title">{toTitleCase(name)}</h4>
-        <div className="description">{description.slice(0, 140)}</div>
-        <div className="keywords">
-          {JSON.parse(keywords).map((keyword, key) => <div key={key} className="keyword">{keyword.text}</div>)}
+export default ({id, name, fullName, track, description, logo, keywords, openModal}) => {
+  return (<div className="project-item" onClick={() => openModal(id)}>
+        <ReactImageFallback
+            src={logo}
+            fallbackImage={placeholder}
+            alt={name}
+            className='icon'
+        />
+        <div className="text-content">
+          <h4 className="title">{toTitleCase(name)} by {toTitleCase(fullName)} - {toTitleCase(track)}</h4>
+          <div className="description">{description.slice(0, 140)}</div>
+          <div className="keywords">
+            {JSON.parse(keywords).map((keyword, key) => <div key={key} className="keyword">{keyword.text}</div>)}
+          </div>
         </div>
-      </div>
-    </div>
-  )
+      </div>)
 }
--- src/components/ProjectsList/index.js
+++ src/components/ProjectsList/index.js
@@ -2,12 +2,11 @@ import React from 'react'
 import axios from 'axios'
 
 import ProjectItem from './components/projectItem'
-import Pagination from "react-js-pagination"
+import Pagination from 'react-js-pagination'
 import ProjectModal from './components/projectModal'
 
 import './Duration.css'
-import { adaAPI } from '../../helpers';
-
+import {adaAPI} from '../../helpers'
 
 class ProjectsList extends React.Component {
 
@@ -24,7 +23,18 @@ class ProjectsList extends React.Component {
     this.fetchProjects()
   }
 
-  renderProjects = projects => projects.map((projectAttribute, key) => <ProjectItem openModal={this.toggleModal} key={key} {...projectAttribute.product} />)
+  renderProjects = projects => projects.map((projectAttribute, key) => {
+    const projectData = {
+      id: projectAttribute.product.id,
+      name: projectAttribute.product.name,
+      description: projectAttribute.product.description,
+      logo: projectAttribute.product.logo,
+      keywords: projectAttribute.product.keywords,
+      fullName: projectAttribute.owner.fullName,
+      track: projectAttribute.track.name,
+    }
+    return <ProjectItem openModal={this.toggleModal} key={key} {...projectData} />
+  })
 
   handlePageChange = pageNumber => {
     this.fetchProjects(pageNumber)
@@ -32,37 +42,38 @@ class ProjectsList extends React.Component {
 
   toggleModal = currentProjectId => {
     if (typeof currentProjectId === 'string') this.fetchProject(currentProjectId)
-    this.setState(state => ({ isOpen: !state.isOpen }))
+    this.setState(state => ({isOpen: !state.isOpen}))
   }
 
   fetchProjects = async (page = 1) => {
     try {
       const response = await axios(adaAPI + '/Products/Paginated/' + page)
-      const { model, pageCount, pageSize, pageNumber } = response.data
+      const {model, pageCount, pageSize, pageNumber} = response.data
       await this.setState({
         products: model,
         pageCount,
         pageSize,
         pageNumber
       })
-    } catch (error) { }
+    } catch (error) {
+    }
   }
 
   fetchProject = async id => {
     try {
       const response = await axios.get(adaAPI + '/Products/' + id)
-      await this.setState({ currentProject: response.data.model })
-    } catch (error) { }
+      await this.setState({currentProject: response.data.model})
+    } catch (error) {
+    }
   }
 
   render() {
-    const { isOpen, currentProject, products, pageCount, pageNumber, pageSize } = this.state;
+    const {isOpen, currentProject, products, pageCount, pageNumber, pageSize} = this.state
 
-    return (
-      <div className='duration-container'>
-        <h3 className="projects-title">Projects</h3>
-        {products && products.length ? this.renderProjects(products) : <p>No Projects</p>}
-        <Pagination
+    return (<div className='duration-container'>
+      <h3 className="projects-title">Projects</h3>
+      {products && products.length ? this.renderProjects(products) : <p>No Projects</p>}
+      <Pagination
           activePage={pageNumber}
           itemsCountPerPage={pageSize}
           totalItemsCount={pageSize * pageCount}
@@ -70,10 +81,9 @@ class ProjectsList extends React.Component {
           linkClass='pagination-item'
           activeLinkClass='pagination-item-active'
           hideFirstLastPages
-        />
-        {isOpen && currentProject && <ProjectModal currentProject={currentProject} toggleModal={this.toggleModal} />}
-      </div>
-    )
+      />
+      {isOpen && currentProject && <ProjectModal currentProject={currentProject} toggleModal={this.toggleModal}/>}
+    </div>)
   }
 }
 
